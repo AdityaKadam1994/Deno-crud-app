dbed9a2c5fa0f8b094821f707630256bdec3e592
Aditya <addy.7kad@gmail.com>
2020-05-25 16:00:20 +0530

    delete route added

--- server.ts
+++ server.ts
@@ -4,10 +4,6 @@ const port = 8080;
 const app = new Application();
 
 app.use(router.routes());
-// app.use(router.allowedMethods());
 
-// await router.get("/api/items", (req, res) => {
-//  res.json("Hello World");
-// });
 console.log("Server running");
 await app.listen({ port });
